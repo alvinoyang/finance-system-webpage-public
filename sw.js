028774b7f090aca2a517fba6cf68f43ff889d43a
alvinoyang <alvinyang@live.ca>
2026-09-16 09:38:15 +0200

    The web page: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE = "finance-system-webpage-ab3d2ae0e816";      // deploy replaces "v8" with a stamp of the page's files
+const CACHE = "finance-system-webpage-7d87479c2540";      // deploy replaces "v8" with a stamp of the page's files
 const FILES = ["./", "index.html", "app.js", "style.css", "config.json", "manifest.webmanifest", "icon.svg"];
 const FRESH_FIRST = new Set(["config.json"]);
 
